b727803a8c00109010c4989458d60fe955c56977
Muhammad Aehtsham Nasir <muhammadaehtsham01@gmail.com>
2025-05-13 17:44:54 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  const mainContent = document.getElementById("main-content");
-
-  setTimeout(() => {
-    loader.style.display = "none";
-    mainContent.classList.remove("hidden");
-  }, 2000); // 2 second loading animation
-});
